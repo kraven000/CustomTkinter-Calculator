9112a4208bf9a362e039858cdfc69931b52acdbf
chotta bheem <akhisanjayverma26@gmail.com>
2024-12-07 23:28:17 +0530

    removing some unnecessary variable from the main.py

--- main.py
+++ main.py
@@ -2,35 +2,33 @@ import customtkinter as ct
 
 
 index = 0
-error = False
 
 def insert_text(events=None):
     '''It is the function which adds the number or mathematical operators in the Entry widget.'''
-    
     global index
-    
+
     if number_expression.get()=="ERROR!":
         entry.delete(0,ct.END)
-    
+
     try:
         if events.char.isdigit() or events.char in {"+","-","*",r"/","."}:
-            if len(number_expression.get())==0 and not error:
+            if len(number_expression.get())==0:
                 index = 0
             else:
                 index += 1
-            
+
             entry.insert(index,events.char)
     except AttributeError:
         if events.isdigit() or events in {"+","-","*",r"/","."}:
             entry.insert(index,events)
-    
+
     index += 1
 
 
 def remove_text(events=None,remove_all=False):
-    '''It is the function which remove the previous charater from entry widget if backspace key is pressed
-    or clear all the charaters from the entry widget if 'CE' button is clicked.'''
-    
+    '''It is the function which remove the previous charater from entry widget if backspace key is 
+    pressed or clear all the charaters from the entry widget if 'CE' button is clicked.'''
+
     prev_text = number_expression.get()
     entry.delete(0,ct.END)
     if remove_all:
@@ -42,12 +40,11 @@ def remove_text(events=None,remove_all=False):
 def evaluate_expression(events=None):
     '''It is the function which evaluate all the expression in the entry widget if enter is pressed or
     '=' is clicked.'''
-    
-    global error
+
     try:
         result = str(eval(number_expression.get()))
         entry.delete(0,ct.END)
-        
+
     except SyntaxError:
         entry.delete(0,ct.END)
         entry.insert(0,"ERROR!")
@@ -64,8 +61,8 @@ if __name__=="__main__":
     window.resizable(False,False)
 
     number_expression = ct.StringVar()
-    entry = ct.CTkEntry(window,textvariable=number_expression,width=377,height=120,font=("Roboto",40),
-                                justify="right")
+    entry = ct.CTkEntry(window,textvariable=number_expression,width=377,height=120,
+                        font=("Roboto",40),justify="right")
     entry.place(x=0,y=1)
 
 
@@ -119,7 +116,7 @@ if __name__=="__main__":
 
 
     button_equal = ct.CTkButton(window,text="=",font=("Roboto",16),width=90,height=60,
-                                        border_width=0,command=lambda:evaluate_expression())
+                                        border_width=0,command=evaluate_expression)
 
     button_backspace = ct.CTkButton(window,text="CE",font=("Roboto",16),width=90,height=60,
                                         border_width=0,command=lambda:remove_text(remove_all=True))
